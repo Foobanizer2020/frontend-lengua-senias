afdbb3cb6a0cc4631a15127af7871838d311fd88
Raúl González <gonzalv@ciencias.unam.mx>
2020-10-28 20:36:33 -0600

    Incializa archivos vacios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/autenticacion/login/login.component';
+import { RegistroComponent } from './components/autenticacion/registro/registro.component';
+import { UsuarioComponent } from './components/autenticacion/usuario/usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistroComponent,
+    UsuarioComponent
   ],
   imports: [
     BrowserModule,
