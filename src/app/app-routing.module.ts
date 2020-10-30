013134b3bd768544ebc30f7560f8bbe5f9a2f167
Raúl González <gonzalv@ciencias.unam.mx>
2020-10-29 18:37:12 -0600

    Base para login y registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/autenticacion/login/login.component';
+import { RegistroComponent } from './components/autenticacion/registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'registro', component:RegistroComponent},
+  {path:'login', component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
