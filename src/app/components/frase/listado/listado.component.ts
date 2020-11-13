20b89572b390433692e2ecbf66572d6a576ef4d0
Raúl González <gonzalv@ciencias.unam.mx>
2020-11-12 19:39:36 -0600

    Listado de frases

--- src/app/components/frase/listado/listado.component.ts
+++ src/app/components/frase/listado/listado.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { Frase } from 'src/app/_models/frase';
+import { FraseService } from 'src/app/_services/frase.service';
+import Swal from 'sweetalert2';
 import { FormularioComponent } from '../formulario/formulario.component';
+
+declare var $: any;
+const messageTime:number = 1000;
 
 @Component({
   selector: 'app-listado-frase',
@@ -10,12 +16,37 @@ export class ListadoComponent implements OnInit {
 
   @ViewChild(FormularioComponent) modalFormulario:FormularioComponent;
 
-  constructor() { }
+  frases:Frase[];
 
-  ngOnInit(): void { }
+  constructor(private fraseService:FraseService) { }
+
+  ngOnInit(): void { 
+    this.getFrases();
+  }
 
   openModal() {
     this.modalFormulario.open();
   }
 
+  getFrases() {
+    this.fraseService.getFrases().subscribe(
+      res => this.frases = res,
+      err => console.log(err)
+    );
+  }
+
+  deleteFrase(id:number) {
+    this.fraseService.deleteFrase(id).subscribe(
+      res => {
+        this.getFrases();
+        Swal.fire({
+          icon: 'success',
+          title: 'Estado eliminado exitosamente',
+          showConfirmButton: false,
+          timer: messageTime
+        });
+      },
+      err => console.log(err)
+    );
+  }
 }
